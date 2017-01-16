677ff71d00165f21f632d76a4d9538f2ee22ce52
Christien Soosaipillai <christien2011@hotmail.ca>
2017-01-16 15:06:40 -0500

    beta branch

--- lab01/Tester.java
+++ lab01/Tester.java
@@ -3,7 +3,7 @@ class Tester {
 	static public void main (String[] args) {
 
 		System.out.println("Hello World");
-		System.out.println("Welcome to CSCI2020u!");
+		System.out.println("Almost done!");
 
 		//Add another line of code here!
 
